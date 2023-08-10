80b76b650ec0880de1a5b4c95b1c0d8474f0153f
yeyupiaoling <yeyupiaoling@foxmail.com>
2023-08-10 23:37:44 +0800

    更新版本号

--- macls/__init__.py
+++ macls/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.4.0"
+__version__ = "0.4.1"
 # 项目支持的模型
 SUPPORT_MODEL = ['EcapaTdnn', 'PANNS_CNN6', 'PANNS_CNN10', 'PANNS_CNN14', 'Res2Net', 'ResNetSE', 'TDNN', 'ERes2Net',
                  'CAMPPlus']
