22cdf96ce9f3e1c40881765fb6695a2027ae386e
Sasha <67932607+placebeyondtheclouds@users.noreply.github.com>
2024-09-28 20:19:19 +0800

    edits

--- train.py
+++ train.py
@@ -8,13 +8,12 @@ parser = argparse.ArgumentParser(description=__doc__)
 add_arg = functools.partial(add_arguments, argparser=parser)
 add_arg('configs',              str,    '../configs/cam++.yml',        '配置文件')
 add_arg('data_augment_configs', str,    '../configs/augmentation.yml', '数据增强配置文件')
-add_arg("local_rank",           int,    0,                          '多卡训练需要的参数')
+add_arg("local_rank",           int,    os.environ['LOCAL_RANK'],                          '多卡训练需要的参数')# by placebeyondtheclouds
 add_arg("use_gpu",              bool,   True,                       '是否使用GPU训练')
 add_arg('save_model_path',      str,    '../models/',                  '模型保存的路径')
 add_arg('log_dir',              str,    '../log/',                     '保存VisualDL日志文件的路径')
 add_arg('resume_model',         str,    None,                       '恢复训练，当为None则不使用预训练模型')
 add_arg('pretrained_model',     str,    None,                       '预训练模型的路径，当为None则不使用预训练模型')
-add_arg("local_rank",       int,    os.environ['LOCAL_RANK'],                          '多卡训练需要的参数') # by placebeyondtheclouds
 args = parser.parse_args()
 print_arguments(args=args)
 
